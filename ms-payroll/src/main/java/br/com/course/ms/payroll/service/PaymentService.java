4a0c6ae6324f87ab08724e75c013cee3bd547225
Cleiton Sena <cflsena2@gmail.com>
2021-05-15 17:39:56 -0300

    Feign Client

--- ms-payroll/src/main/java/br/com/course/ms/payroll/service/PaymentService.java
+++ ms-payroll/src/main/java/br/com/course/ms/payroll/service/PaymentService.java
@@ -1,28 +1,19 @@
 package br.com.course.ms.payroll.service;
 
+import br.com.course.ms.payroll.client.WorkerFeignClient;
 import br.com.course.ms.payroll.domain.Payment;
 import br.com.course.ms.payroll.domain.Worker;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
-    private final RestTemplate restTemplate;
+    private final WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(Long workerId, int days) {
-        Map<String, String> uriVariable = new HashMap<>();
-        uriVariable.put("id", "" + workerId);
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return Payment.create(worker.getName(), worker.getDailyIncome(), days);
     }
 
