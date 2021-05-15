49d9d08c6e593cd9ad8924023385b79ab1061b28
Cleiton Sena <cflsena2@gmail.com>
2021-05-15 18:19:13 -0300

    Ribbon load balacing

--- ms-worker/src/main/java/br/com/course/ms/worker/controller/WorkerController.java
+++ ms-worker/src/main/java/br/com/course/ms/worker/controller/WorkerController.java
@@ -3,6 +3,8 @@ package br.com.course.ms.worker.controller;
 import br.com.course.ms.worker.domain.Worker;
 import br.com.course.ms.worker.repository.WorkerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/workers")
 @RequiredArgsConstructor
 public class WorkerController {
 
     private final WorkerRepository workerRepository;
+    private final Environment env;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
@@ -25,6 +29,7 @@ public class WorkerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        log.info("PORT: " + env.getProperty("local.server.port"));
         return ResponseEntity.ok(workerRepository.findById(id).get());
     }
 }
