37e141c2f9602188715db6a2defd8d631f43419b
Cleiton Sena <cflsena2@gmail.com>
2021-05-16 13:36:55 -0300

    oauth-server project created

---
+++ oauth-server/src/test/java/br/com/course/ms/oauth/server/OauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course.ms.oauth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
