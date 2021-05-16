5d5d2eef76b33f4ef81faadeeb994098c531f7e5
Cleiton Sena <cflsena2@gmail.com>
2021-05-15 23:33:45 -0300

    Project api-gateway-zuul created

---
+++ api-gateway-zuul/src/main/java/br/com/course/ms/api/gateway/zuul/ApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.course.ms.api.gateway.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ api-gateway-zuul/src/test/java/br/com/course/ms/api/gateway/zuul/ApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course.ms.api.gateway.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
