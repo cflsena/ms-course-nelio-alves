87119c803833c20499b835dc505961c097def81a
Cleiton Sena <cflsena2@gmail.com>
2021-05-16 11:10:29 -0300

    Configuring Config Server Project

---
+++ config-server/src/test/java/r/com/course/ms/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package r.com.course.ms.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
